db5886b849037451df1e9f29612cbb83a6da40ce
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2026-03-17 20:38:12 +0100

    Adapt database plan

--- src/db/schema/field_conditions.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
-
-export const fieldConditions = pgTable('field_conditions', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 100 }).notNull().unique(),
-  effect: text('effect'),
-});
