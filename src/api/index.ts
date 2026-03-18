0f0c8275e6a39afc629c1b2e3656d13cb9d0e0a5
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2026-03-18 20:10:00 +0100

    Init API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,9 @@
-export {}
+import { Hono } from 'hono';
+import pokemonRouter from './pokemon';
+
+const app = new Hono();
+
+const route = app.route('/api/pokemon', pokemonRouter);
+
+export type AppType = typeof route;
+export default app;
--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,1 +1,28 @@
-export {}
+import { Hono } from 'hono';
+import { db } from '../db/client';
+import { pokemon } from '../db/schema';
+import { eq } from 'drizzle-orm';
+
+const app = new Hono();
+
+const route = app.get('/:id', async (c) => {
+  const id = Number(c.req.param('id'));
+
+  if (Number.isNaN(id)) {
+    return c.json({ error: 'Invalid ID' }, 400);
+  }
+
+  const [found] = await db
+    .select()
+    .from(pokemon)
+    .where(eq(pokemon.id, id));
+
+  if (!found) {
+    return c.json({ error: 'Pokemon not found' }, 404);
+  }
+
+  return c.json(found);
+});
+
+export type PokemonRouteType = typeof route;
+export default app;
